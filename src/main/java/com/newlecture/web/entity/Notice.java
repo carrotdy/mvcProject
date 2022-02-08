ee2a0befca1552af3fd5c6c8c43759d8f5e285d7
DESKTOP-C55UU5R\홍다영 <dyhong9212@gmail.com>
2022-02-08 22:09:39 +0900

    22.02.08 NoticeService(JDBC연결) dispatcher-servlet.xml파일분리 (security,service,servlet)

--- src/main/java/com/newlecture/web/entity/Notice.java
+++ src/main/java/com/newlecture/web/entity/Notice.java
@@ -3,7 +3,7 @@ package com.newlecture.web.entity;
 import java.util.Date;
 
 public class Notice {
-	private int id;
+	private int no;
 	private String title;
 	private String writerId;
 	private Date regDate;
@@ -11,12 +11,20 @@ public class Notice {
 	private int hit;
 	private String files;
 	
-	public Notice() {
-		super();
+	public void print(){
+		System.out.printf("%d %s %s %s %d %s\n",
+				this.no,
+				this.title,
+				this.writerId,
+				this.content,
+				this.hit,
+				this.files);
 	}
-	public Notice(int id, String title, String writerId, Date regDate, String content, int hit, String files) {
-		super();
-		this.id = id;
+	
+	public Notice() {}
+
+	public Notice(int no, String title, String writerId, Date regDate, String content, int hit, String files) {
+		this.no = no;
 		this.title = title;
 		this.writerId = writerId;
 		this.regDate = regDate;
@@ -24,46 +32,62 @@ public class Notice {
 		this.hit = hit;
 		this.files = files;
 	}
-	public int getId() {
-		return id;
+
+	public int getno() {
+		return no;
 	}
-	public void setId(int id) {
-		this.id = id;
+
+	public void setId(int no) {
+		this.no = no;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getWriterId() {
 		return writerId;
 	}
+
 	public void setWriterId(String writerId) {
 		this.writerId = writerId;
 	}
+
 	public Date getRegDate() {
 		return regDate;
 	}
+
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
+
 	public String getContent() {
 		return content;
 	}
+
 	public void setContent(String content) {
 		this.content = content;
 	}
+
 	public int getHit() {
 		return hit;
 	}
+
 	public void setHit(int hit) {
 		this.hit = hit;
 	}
+
 	public String getFiles() {
 		return files;
 	}
+
 	public void setFiles(String files) {
 		this.files = files;
-	}	
+	}
 }
+
+
--- src/main/java/com/newlecture/web/service/NoticeService.java
+++ src/main/java/com/newlecture/web/service/NoticeService.java
@@ -1,58 +1,115 @@
 package com.newlecture.web.service;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.newlecture.web.entity.Notice;
 
 public class NoticeService {
-	private String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
-	private String uid = "NOTICE";
-	private String pwd = "NOTICE";
-	private String driver = "oracle.jdbc.driver.OracleDriver";
+	/*
+	 * private String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe"; private String
+	 * uid = "NOTICE"; private String pwd = "NOTICE"; private String driver =
+	 * "oracle.jdbc.driver.OracleDriver";
+	 */
+	
+	@Autowired
+	private DataSource dataSource;
 
 	public List<Notice> getList(int page, String field, String query) throws ClassNotFoundException, SQLException {
 		int start = 1 + (page - 1) * 10; // 1page 1, 2page 11, 3page 21... 등차수열 a1 + (n-1)*d
 		int end = 10 * page; // 1page 10, 2page 20, 3page 30...
 
-		String sql = "SELECT * FROM NOTICE WHERE "+field+" LIKE ? AND NUM BETWEEN ? AND ?";
+		String sql = "SELECT * FROM NOTICE";
 
-		Class.forName(driver);
-		Connection con = DriverManager.getConnection(url, uid, pwd);
+		/*
+		 * Class.forName(driver); 
+		 * Connection con = DriverManager.getConnection(url, uid, pwd);
+		 */
+		Connection con = dataSource.getConnection();
 		PreparedStatement st = con.prepareStatement(sql);
-		st.setString(1, "%"+query+"%");
-		st.setInt(2, start);
-		st.setInt(3, end);
+		
 		ResultSet rs = st.executeQuery();
 
 		List<Notice> list = new ArrayList<Notice>();
 
 		while (rs.next()) {
-			int id = rs.getInt("ID");
-			String title = rs.getString("TITLE");
-			String writerId = rs.getString("WRITER_ID");
-			String regDate = rs.getString("REGDATE");
-			String content = rs.getString("CONTENT");
-			int hit = rs.getInt("hit");
+			int no = rs.getInt("NO");
+			String title=rs.getString("TITLE");
+			String writerId=rs.getString("WRITER_ID");
+			Date regDate=rs.getDate("REGDATE");
+			String content=rs.getString("CONTENT");
+			int hit= rs.getInt("HIT");
 			String files = rs.getString("FILES");
 
-			Notice notice = new Notice(id, title, writerId, null, content, hit, files);
+			Notice notice = new Notice(no, title, writerId, regDate, content, hit, files);
 
 			list.add(notice);
 		}
-
 		st.close();
 		con.close();
 		rs.close();
 
 		return list;
-
 	}
+	
+	public int getCount() throws ClassNotFoundException, SQLException{
+		int count = 0;
+		String sql = "SELECT COUNT(no) COUNT FROM NOTICE";
+		
+		/*
+		 * Class.forName(driver); 
+		 * Connection con = DriverManager.getConnection(url, uid, pwd);
+		 */
+		Connection con = dataSource.getConnection();
+		Statement st = con.createStatement();
+		
+		ResultSet rs = st.executeQuery(sql);
+		
+		if(rs.next()) {
+			count = rs.getInt("COUNT");
+		}
+		rs.close();
+		st.close();
+		con.close();
 
+		return count;
+	}
+	
+	public int insert(Notice notice) throws ClassNotFoundException, SQLException{
+		String title = notice.getTitle();
+		String writerId = notice.getWriterId();
+		String content = notice.getContent();
+		String files = notice.getFiles();
+		
+		String url = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+		String sql = "INSERT INTO NOTICE ( "+" title," + " writer_id," + "content," + "files" + ") VALUES(?,?,?,?)" ;
+		
+		/*
+		 * Class.forName(driver); 
+		 * Connection con = DriverManager.getConnection(url, uid, pwd);
+		 */
+		Connection con = dataSource.getConnection();
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1,title);
+		st.setString(2, writerId);
+		st.setString(3, content);
+		st.setString(4, files);
+		
+		int result = st.executeUpdate();
+		
+		st.close();
+		con.close();
+		
+		return result;
+	}
 }
