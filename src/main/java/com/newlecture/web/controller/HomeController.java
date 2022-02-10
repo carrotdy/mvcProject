f1c13d817aa18c111a7a61927822fbbea9fa5d11
DESKTOP-C55UU5R\홍다영 <dyhong9212@gmail.com>
2022-02-11 00:09:28 +0900

    22.02.10 - @RestController, 한글출력 설정 - Json출력

--- src/main/java/com/newlecture/web/controller/HomeController.java
+++ src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,15 +1,37 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class HomeController{
 
+	/*
+	 * @RequestMapping("index") 
+		 * public void index(HttpServletResponse response) {
+		 * 	PrintWriter out; 
+		 *  try { 
+		 *      out = response.getWriter();
+		 *      out.println("Hello Index"); 
+		 *  } catch (IOException e) { 
+		 *      // TODO Auto-generated catch block 
+		 *    e.printStackTrace(); 
+		 *  } 
+		 *  //return "root.index"; 
+	 *  }
+	 */
+	
 	@RequestMapping("index")
+	@ResponseBody
 	public String index() {
-		return "root.index";
+		return "Hello Index 1";
 	}
 	
 	
